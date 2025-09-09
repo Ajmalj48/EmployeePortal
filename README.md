bff4eb42a9871215ac85b1f5fa2c5f86d052c333
Ajmal J <109408654+Ajmalj48@users.noreply.github.com>
2025-09-09 13:23:14 +0530

    Added Migrations

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # EmployeePortal
 Manage Employee details
+
+# EF
+cd src/EmployeePortal.Api
+dotnet ef migrations add InitialCreate -p ../EmployeePortal.Infrastructure -s . -o Persistence/Migrations
+dotnet ef database update -p ../EmployeePortal.Infrastructure -s .
---
+++ src/EmployeePortal.Infrastructure/Persistence/Migrations/20250909074556_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeePortal.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    HireDate = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
